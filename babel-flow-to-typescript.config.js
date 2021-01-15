7ea2072caec0edfd60e094a9b67dfe47c83220aa
Jennifer Shehane <jennifer@cypress.io>
2021-01-15 11:32:06 +0630

    init repo that reproduces preprocessor crash

---
+++ babel-flow-to-typescript.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  plugins: [
+    "@babel/plugin-syntax-flow",
+    "@babel/plugin-syntax-jsx",
+    "@babel/plugin-syntax-optional-chaining",
+    "@babel/plugin-syntax-nullish-coalescing-operator",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-syntax-class-properties",
+    "babel-plugin-flow-to-typescript",
+  ],
+};
